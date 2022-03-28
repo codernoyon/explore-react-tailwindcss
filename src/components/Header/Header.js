5029b184e7252ac2edc9475d9870354b7e5c46fc
codernoyon <noyonrahman2003@gmail.com>
2022-03-28 21:57:06 +0600

    logo update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,9 +15,7 @@ const Header = () => {
     return (
         <header className='bg-indigo-400'>
             <nav className='w-full px-4 py-3 lg:px-0  lg:w-5/6 mx-auto flex items-center justify-between z-10 lg:z-0'>
-                <div className="">
-                    <a href="#" className="text-3xl font-semibold">Logo</a>
-                </div>
+                <div className="text-3xl font-semibold">Logo</div>
                 <ul className={`w-full py-3 lg:py-0 absolute bg-indigo-300 lg:bg-inherit lg:static lg:w-auto  left-0  lg:flex -z-10 lg:z-0 duration-300 ease-in  ${ open? 'top-14': 'top-[-80px]'}` }>
                     {       
                         routes.map(rout => <Rout
